9109d8f311ebba0472b3b1e086b633feac1c62e8
FabioMiguelNascimento <fabiomnascimento05@gmail.com>
2025-07-10 14:53:49 -0300

    chore: Adicionado handling de error para caso nao seja ponto de coleta do usuario

--- src/application/use-cases/collection-points/EditCollectionPoint.js
+++ src/application/use-cases/collection-points/EditCollectionPoint.js
@@ -1,13 +1,19 @@
-import { ConflictError } from "../../../errors/HttpErrors.js";
+import { ConflictError, UnauthorizedError } from "../../../errors/HttpErrors.js";
 
 export default function makeEditCollectionPoint(collectionRepository) {
   return async function editCollectionPoint({data, userId, collectionId}) {
     const existingCollectionPoint = await collectionRepository.findById(collectionId);
+    
     if (!existingCollectionPoint) {
       throw new ConflictError("Ponto de coleta não encontrado com o id fornecido.");
     }
 
+    if (existingCollectionPoint.userId !== userId) {
+      throw new UnauthorizedError("Você não tem permissão para editar este ponto de coleta.");
+    }
+
     const editCollectionPoint = await collectionRepository.edit({data, userId, collectionId});
-    return editCollectionPoint;
+    const { userId: _, ...editData } = editCollectionPoint;
+    return editData;
   };
 }
