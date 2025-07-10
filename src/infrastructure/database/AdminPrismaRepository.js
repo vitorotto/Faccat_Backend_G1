c617df624f39459bf26ce49ed93389e67ff14f19
FabioMiguelNascimento <fabiomnascimento05@gmail.com>
2025-07-10 15:11:15 -0300

    refc: refeito conexoes com o banco para usar a instancia do prisma e nao perder ela no finally

--- src/infrastructure/database/AdminPrismaRepository.js
+++ src/infrastructure/database/AdminPrismaRepository.js
@@ -1,24 +1,18 @@
-import { PrismaClient } from "../../../generated/client/index.js";
+import { prisma } from "./prisma.js";
 import AdminRepository from "../../domain/ports/AdminRepository.js";
-
-const prisma = new PrismaClient()
 
 export default class AdminPrismaRepository extends AdminRepository{
     async validateCollectionPoint(id){
-        try {
-            const validated = await prisma.colectionPoints.update({
-                where: {
-                    id: id,
-                    validated: false,
-                },
-                data: {
-                    validated: true,
-                    updatedAt: new Date()
-                }
-            })
-            return validated
-        } finally {
-            await prisma.$disconnect()
-        }
+        const validated = await prisma.colectionPoints.update({
+            where: {
+                id: id,
+                validated: false,
+            },
+            data: {
+                validated: true,
+                updatedAt: new Date()
+            }
+        })
+        return validated
     }
 }
