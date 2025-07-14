eea60f8e4b5262e66651df28df15dbfa3219887b
vitorotto <vitorotto05@gmail.com>
2025-07-14 12:38:42 -0300

    chore: Inicio da criaçãodas rotas para Tarefas de Descarte

--- src/infrastructure/http/routes/discartTasksRoutes.js
+++ src/infrastructure/http/routes/discartTasksRoutes.js
@@ -1,1 +1,15 @@
-i
+import { Router } from 'express';
+import { validateToken } from '../../../middlewares/authMiddleware.js';
+import { validateRequest } from '../../../middlewares/validateRequestMiddleware.js';
+import { createDiscartTask, getDiscartTasks, updateDiscartTask, deleteDiscartTask } from '../../controllers/discartTasksController.js';
+
+const discartTasksRoutes = Router();
+
+discartTasksRoutes.post('/create', validateToken, createDiscartTask);
+discartTasksRoutes.get('/list', getDiscartTasks);
+discartTasksRoutes.put('/update/:id', updateDiscartTask);
+discartTasksRoutes.delete('/delete/:id', deleteDiscartTask);
+
+export default discartTasksRoutes;
+
+
